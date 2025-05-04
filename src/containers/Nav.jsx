34ba9ef0f6e343fa06483aadc890766871038811
shahriyortoshmurodov <shahriyor1247@gmail.com>
2025-05-04 18:13:01 +0500

    Yangi

--- src/containers/Nav.jsx
+++ src/containers/Nav.jsx
@@ -6,10 +6,10 @@ const Nav = () => {
             <div className="flex items-center justify-between ">
                 <h1 className='text-3xl font-semibold text-[#1e69ff]'>ShahriyorDev</h1>
                 <div className="flex items-center justify-between gap-10">
-                    <ul className="hidden md:flex items-center justify-between gap-10">
-                        <li><a href="#home" className="text-gray-600 hover:text-[#1e69ff] text-xl inline-block transform transition duration-200 hover:-translate-y-1">Home</a></li>
-                        <li><a href="#skills" className="text-gray-600 hover:text-[#1e69ff] text-xl inline-block transform transition duration-200 hover:-translate-y-1">Skills</a></li>
-                        <li><a href="#projects" className="text-gray-600 hover:text-[#1e69ff] text-xl inline-block transform transition duration-200 hover:-translate-y-1">Projects</a></li>
+                    <ul className=" flex items-center justify-between gap-10">
+                        <li className='hidden md:flex' ><a href="#home" className="text-gray-600 hover:text-[#1e69ff] text-xl inline-block transform transition duration-200 hover:-translate-y-1">Home</a></li>
+                        <li className='hidden md:flex' ><a href="#skills" className="text-gray-600 hover:text-[#1e69ff] text-xl inline-block transform transition duration-200 hover:-translate-y-1">Skills</a></li>
+                        <li className='hidden md:flex' ><a href="#projects" className="text-gray-600 hover:text-[#1e69ff] text-xl inline-block transform transition duration-200 hover:-translate-y-1">Projects</a></li>
                         <li><a href="#contact" className="text-gray-600 hover:text-[#1e69ff] text-xl inline-block transform transition duration-200 hover:-translate-y-1">Contact</a></li>
                     </ul>
                    
